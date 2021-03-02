2ea831f031794c4c6407a61b7c1d08521c55a460
KyleBCox <kylecoxforweb@gmail.com>
2021-03-02 22:47:59 +0000

    initial commit

---
+++ src/main/java/com/ticticboooom/mods/rokkit/Rokkit.java
@@ -0,0 +1,12 @@
+package com.ticticboooom.mods.rokkit;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(Rokkit.ID)
+public class Rokkit {
+    public static final String ID = "rokkit";
+
+    public Rokkit() {
+
+    }
+}
